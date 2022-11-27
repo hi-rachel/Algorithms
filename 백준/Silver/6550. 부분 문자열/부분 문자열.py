1d0ff7464e1416f291c8cf756c14d96b50fc8b1e
Rachel <rachel.uiux@gmail.com>
2022-11-28 00:29:10 +0900

    [Silver V] Title: 부분 문자열, Time: 104 ms, Memory: 32424 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -0,0 +1,18 @@
+import sys
+from collections import deque
+while True:
+  try:
+    s, t = input().split()
+    queue = deque(list(s))
+
+    for p in t:
+      if queue and p == queue[0]:
+        queue.popleft()
+
+    if queue:
+      print('No')
+    else:
+      print('Yes')
+      ''
+  except:
+    break
