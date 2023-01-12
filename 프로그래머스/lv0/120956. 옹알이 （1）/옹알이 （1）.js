250d8ddec2e88469ae11ea57055ed51a37809450
Rachel <rachel.uiux@gmail.com>
2023-01-12 21:16:55 +0900

    [level 0] Title: 옹알이 (1), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,13 @@
+function solution(babbling) {
+    let result = 0;
+    
+    for (let babyTalk of babbling) {
+        if (/(aya | ye | woo | ma)\1+/g.test(babyTalk)) continue;
+        
+        if (/^(aya|ye|woo|ma)+$/g.test(babyTalk)) {
+            result++;
+        };
+    }
+    return result;
+}
+    
