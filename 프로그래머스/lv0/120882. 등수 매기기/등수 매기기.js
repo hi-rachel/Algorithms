69d29183ea9c1bf102c5687fef5c4fe4d7822246
Rachel <rachel.uiux@gmail.com>
2023-01-12 19:34:53 +0900

    [level 0] Title: 등수 매기기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -3,7 +3,7 @@ function solution(score) {
     for (let test in score) {
             result.push(score[test].reduce((a,b) => (a + b / 2), 0));
         }
-        const sorted = result.slice().sort((a, b) => b - a);
-        const rank = result.slice().map((v) => sorted.indexOf(v)+1);
+        const sorted = [...result].sort((a, b) => b - a);
+        const rank = [...result].map((v) => sorted.indexOf(v)+1);
         return rank;
 }
