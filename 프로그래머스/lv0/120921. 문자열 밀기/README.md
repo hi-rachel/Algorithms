7248880202890bc7ad43c358d7e68d1031ecf1c6
Rachel <rachel.uiux@gmail.com>
2023-01-14 21:43:59 +0900

    [level 0] Title: 문자열 밀기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/README.md
+++ 프로그래머스/lv0/120921. 문자열 밀기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 밀기 - 120921 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120921?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33 MB, 시간: 0.15 ms
+메모리: 33.5 MB, 시간: 0.17 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,14 +1,13 @@
 function solution(A, B) {
     let arr = [...A];
+    if (A === B) {
+        return 0;
+    }
     for(let i = 0; i < arr.length; i++) {
-        if (A === B) {
-            return i;
-        } else {
             arr.unshift(arr.pop())
             if (arr.join('') === B) {
                 return i + 1;
             }
         }
-    }
     return -1;
 }
