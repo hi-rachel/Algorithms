cda395ce892117c3ceb1fbf74fc07d14f0a196d7
Rachel <rachel.uiux@gmail.com>
2023-01-15 22:48:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,14 +1,3 @@
 function solution(s){
-    const pRegex = /p/gi;
-    const yRegex = /y/gi;
-    let pCount = 0;
-    let yCount = 0;
-    
-    if (s.includes('p') | s.includes('P')) {
-        pCount = s.match(pRegex).length;
-    }
-    if (s.includes('y') | s.includes('Y')) {
-        yCount = s.match(yRegex).length;
-    }
-    return pCount === yCount ? true : false;
+    return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
 }
