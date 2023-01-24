907147972e4492cf473a301ec1d2d969da0181aa
Rachel <rachel.uiux@gmail.com>
2023-01-24 20:45:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 8.97 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,13 +1,14 @@
 function solution(s) {
-    let deleteZero = 0;
-    let count = 0;
-    while(s.length !== 1) {
-        const originLen = s.length;
-        s = s.split('').filter(v => v === '1').join('');
-        const len = s.length
-        deleteZero += originLen - len
-        s = len.toString(2)
-        count++
+    let answer = [0, 0];
+    while (s.length > 1) {
+        // 이진 변환 횟수++
+        answer[0]++;
+        
+        // s 안에 0의 갯수++
+        answer[1] += (s.match(/0/g)||[]).length;
+        
+        // 0을 제거한 길이 이진 변환
+        s = s.replace(/0/g, '').length.toString(2);
     }
-    return[count, deleteZero];
+    return answer;
 }
