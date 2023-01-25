7472c10fcb298943be97ecd18e9d31aaa74e6a23
Rachel <rachel.uiux@gmail.com>
2023-01-25 18:29:12 +0900

    [level 2] Title: 가장 큰 수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,4 +1,16 @@
+// function solution(numbers) {
+//     // const answer = numbers.sort((a,b) => `${b}${a}` - `${a}${b}`);
+//     const answer = numbers.sort((a,b) => `${a}${b}` - `${b}${a}`);
+//     return answer[0] === 0 ? ""+answer[0] : answer.join('');
+// }
+
 function solution(numbers) {
-    const answer = numbers.sort((a,b) => `${b}${a}` - `${a}${b}`);
-    return answer[0] === 0 ? ""+answer[0] : answer.join('');
-}
+    numbers.sort((a,b)=>{
+        let sa = a.toString();
+        let sb = b.toString();
+        return parseInt(sa + sb) > parseInt(sb + sa) ? -1 : 1;
+    })
+    if(numbers[0] === 0) return '0'
+    
+    return numbers.join('')
+};
