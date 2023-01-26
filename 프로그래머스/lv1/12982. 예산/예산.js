1879e42ed944cfc5a454ab6b3967e914b2ff5c0a
Rachel <rachel.uiux@gmail.com>
2023-01-26 23:16:08 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,11 +1,5 @@
 function solution(d, budget) {
-    let count = 0;
-    let sorted = d.sort((a,b) => (a-b));
-
-    for (let n of sorted) {
-        budget -= n
-        if (budget < 0) break;
-        count++;
-    }
-    return count;
+    return d.sort((a, b) => a - b).reduce((count, price) => {
+        return count + ((budget -= price) >= 0);
+    }, 0);
 }
