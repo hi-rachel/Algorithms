62413cc7b3f9384be3d8cb7fa8192098e93b8cd2
Rachel <rachel.uiux@gmail.com>
2023-02-02 23:30:40 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,15 +1,11 @@
 function solution(number) {
-    let answer = 0;
-    
-    const three = (current, start) => {
-        if (current.length === 3) {
-            answer += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
-            return;
-        }
-        for (let i = start; i < number.length; i++) {
-            three([...current, number[i]], i+1);
+    let cnt = 0;
+    for (let i = 0; i < number.length-2; i++) {
+        for (let j = i+1; j < number.length-1; j++) {
+            for (let k = j+1; k < number.length; k++) {
+                if (number[i] + number[j] + number[k] === 0) cnt++;
+            }
         }
     }
-    three([], 0);
-    return answer;
+    return cnt;
 }
