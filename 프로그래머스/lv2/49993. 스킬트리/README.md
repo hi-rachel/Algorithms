a67d01bf2de80cbb9a0ee95391be4147569aba52
Rachel <rachel.uiux@gmail.com>
2023-02-09 16:29:10 +0900

    [level 2] Title: 스킬트리, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/README.md
+++ 프로그래머스/lv2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.21 ms
+메모리: 33.5 MB, 시간: 0.21 ms
 
 ### 구분
 
--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.js
@@ -1,10 +1,13 @@
 function solution(skill, skill_trees) {
-    let answer = 0;
-    let regex = new RegExp(`[^${skill}]`, 'g');
-    
-    return skill_trees
-        .map((x) => x.replace(regex, ''))
-        .filter((x) => {
-            return skill.indexOf(x) === 0 || x === "";
-        }).length;
+    function isCorrect(n) {
+        let test = skill.split('');
+        for (var i = 0; i < n.length; i++) {
+            if (!skill.includes(n[i])) continue;
+            if (n[i] === test.shift()) continue;
+            return false;
+        }
+        return true;
+    }    
+
+    return skill_trees.filter(isCorrect).length;
 }
