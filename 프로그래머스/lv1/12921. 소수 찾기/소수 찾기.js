c38beb9f2a3dee45a98f8fd1aa2b438c2443af46
Rachel <rachel.uiux@gmail.com>
2023-02-11 18:04:12 +0900

    [level 1] Title: 소수 찾기, Time: 77.08 ms, Memory: 55.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+    let result = new Array(n);
+    if (n == 2) return 1;
+    // n 범위
+    for (let i = 2; i <= n; i++) {
+        result[i] = i;
+    }
+    
+    for (let i = 2; i <= n; i++) {
+        if(result[i] == 0) continue;
+        // 2부터 시작 특정 수의 배수에 해당하는 수는 모두 지워줌
+        for (let j = 2*i; j <= n; j += i) {
+            result[j] = 0;
+        }
+    } 
+    return result.filter(x => x !== 0).length
+}
