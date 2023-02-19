4548ec1cc0c20014d89a100b86d99f171b5d8d43
Rachel <rachel.uiux@gmail.com>
2023-02-19 17:25:13 +0900

    [level 2] Title: [1차] 캐시, Time: 1.42 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -5,15 +5,19 @@ function solution(cacheSize, cities) {
     let answer = 0;
     let cityArr = [];
     
+    // cities의 각각 city 실행
     for (let i = 0; i < cities.length; i++) {
         let current = cities[i].toLowerCase();
         let findCity = cityArr.find((city) => city === current);
+        // cityArr에 현재 city가 존재하지 않는다면 추가
         if (!findCity) {
             cityArr.push(current);
+            // 캐시크기보다 커지면 맨 앞 캐시 삭제 => cache miss => 실행시간 + 5
             if (cityArr.length > cacheSize) {
                 cityArr.shift();
             }
             answer += 5;
+            // cityArr에 현재 city가 존재한다면 해당 city 제거 후 마지막에 city 넣어줌 => cache hit => 실행시간 + 1
         } else {
             cityArr = cityArr.filter((city) => city !== current);
             cityArr.push(current);
