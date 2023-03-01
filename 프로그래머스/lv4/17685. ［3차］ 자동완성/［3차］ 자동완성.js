57f4fa6087fbfdb74ae9723ecca226330cba5865
Rachel <rachel.uiux@gmail.com>
2023-03-01 19:35:50 +0900

    [level 4] Title: [3차] 자동완성, Time: 1417.59 ms, Memory: 306 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value = '') {
+        this.value = value;
+        this.children = new Map();
+        this.cnt = 0;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new Node();
+    }
+    insert(string) {
+        let currentNode = this.root;
+        currentNode.cnt += 1;
+        
+        for (const char of string) {
+            if (!currentNode.children.has(char)) {
+                currentNode.children.set(char, new Node(currentNode.value + char));
+            }
+            currentNode = currentNode.children.get(char);
+            currentNode.cnt += 1;
+        }
+    }
+    getCount(string) {
+        let cnt = 0;
+        let currentNode = this.root;
+        for (const char of string) {
+            cnt += 1;
+            
+            currentNode = currentNode.children.get(char);
+            if (currentNode.cnt === 1) break;
+        }
+        return cnt;
+    }
+}
+const solution = (words) => {
+    let result = 0;
+    const trie = new Trie();
+    
+    for (let i = 0; i < words.length; i += 1) {
+        trie.insert(words[i]);
+    }
+    for (let i = 0; i < words.length; i += 1) {
+        result += trie.getCount(words[i]);
+    }
+    return result;
+}
