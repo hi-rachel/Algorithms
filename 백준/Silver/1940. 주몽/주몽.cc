f76143666ad437b10d1e6a9e92b8d30bf9a618e4
Rachel <rachel.uiux@gmail.com>
2023-04-11 22:04:24 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[15004], cnt;
+int n, m, a, g[15004], ans;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
     for(int i = 0; i < n; i++){
-        cin >> a[i];
+        cin >> g[i];
     }
     for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
-            if(a[i] + a[j] == m){
-                cnt++;
+            if(g[i] + g[j] == m){
+                ans++;
             }
         }
     }
-    cout << cnt << '\n';
+    cout << ans << '\n';
     return 0;
 }
