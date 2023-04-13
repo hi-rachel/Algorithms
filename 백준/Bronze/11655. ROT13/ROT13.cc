29e0a0732594c0584fc523ff351f0807ebb42fad
Rachel <rachel.uiux@gmail.com>
2023-04-13 21:02:17 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -5,13 +5,13 @@ int main(){
     getline(cin, s);
     for(int i = 0; i < s.size(); i++){
         if(s[i] >= 65 && s[i] <= 90){
-            if(s[i] + 13 > 90) s[i] += 13 - 26;
+            if(s[i] + 13 > 90) s[i] -= 13;
             else s[i] += 13;
-        } else if(s[i] >= 97 && s[i] <= 122){
-            if(s[i] + 13 > 122) s[i] += 13 - 26;
+        }else if(s[i] >= 97 && s[i] <= 122){
+            if(s[i] + 13 > 122) s[i] -= 13;
             else s[i] += 13;
         }
+        cout << s[i];
     }
-    cout << s << '\n';
     return 0;
 }
