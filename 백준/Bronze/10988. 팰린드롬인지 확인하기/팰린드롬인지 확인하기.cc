67bdd4324fb2a0b5c2ee1bce63cc034ab19749e7
Rachel <rachel.uiux@gmail.com>
2023-04-13 16:04:13 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-string word, temp;
+string s, temp;
 int main(){
-    cin >> word;
-    temp = word;
+    cin >> s;
+    temp = s;
     reverse(temp.begin(), temp.end());
-    if(word == temp) cout << 1 << '\n';
+    if(s == temp)
+        cout << 1 << '\n';
     else cout << 0 << '\n';
     return 0;
 }
