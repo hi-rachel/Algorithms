c5f77a74942f5abbef407ee785704c43227cd11d
Rachel <rachel.uiux@gmail.com>
2023-04-14 14:50:28 +0900

    [unrated] Title: 추억 점수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,16 @@
+function solution(name, yearning, photo) {
+    let result = [];
+    for(let i = 0; i < photo.length; i++){
+        let test = photo[i];
+        let ans = 0;
+        for(let j = 0; j < test.length; j++){
+            let idx = name.indexOf(test[j]);
+            if(idx >= 0) {
+                ans += yearning[idx];
+            }
+        }
+        result.push(ans);
+        ans = 0;
+    }
+    return result;
+}
