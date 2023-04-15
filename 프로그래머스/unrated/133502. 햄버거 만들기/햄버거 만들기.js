9fd60d39a1c399d9103e40102503d3e2fdf79e5b
Rachel <rachel.uiux@gmail.com>
2023-04-16 00:42:10 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,19 +1,12 @@
 function solution(ingredient) {
     // 1 2 3 1 이 되면 햄버거 완성
-    const stack = [];
     let cnt = 0;
-    ingredient.forEach((ing, idx)=>{
-        stack.push(ing);
-        if(stack.length >= 4){
-            const find = stack.slice(-4).join('');
-            if(find === '1231'){
-                stack.pop();
-                stack.pop();
-                stack.pop();
-                stack.pop();
-                cnt++;
-            }
+    for(let i = 0; i < ingredient.length; i++){
+        if(ingredient.slice(i, i+4).join('') === '1231'){
+            cnt++;
+            ingredient.splice(i, 4);
+            i -= 3;
         }
-    })
+    }
     return cnt;
 }
