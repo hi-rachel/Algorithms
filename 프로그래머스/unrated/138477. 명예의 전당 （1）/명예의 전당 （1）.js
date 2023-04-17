01076804c7b260249929214007f89dd3fbcfec6f
Rachel <rachel.uiux@gmail.com>
2023-04-17 21:42:31 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,7 +1,6 @@
 function solution(k, score) {
     let high = [];
     let result = [];
-    let rank = high.sort((a,b) => a-b);
     for(let i = 0; i < score.length; i++){
         high.push(score[i]);
         high.sort((a,b) => a-b);
