7eb1ccc2485f5a6be6a196cf173c4021d5e70278
Rachel <rachel.uiux@gmail.com>
2023-06-03 11:40:34 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.47 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -2,40 +2,30 @@ function solution(survey, choices) {
     let result = '';
     
     let scores = {
-        // 1번 지표
         'R': 0,
         'T': 0,
-        // 2번 지표
         'C': 0,
         'F': 0,
-       // 3번 지표
         'J': 0,
         'M': 0,
-        // 4번 지표
         'A': 0,
         'N': 0,
     }
     
     for(let i = 0; i < choices.length; i++){
         if(choices[i] < 4) {
-            // 비동의
             scores[survey[i].split('')[0]] += Math.abs(choices[i] - 4);
         } else if (choices[i] > 4) {
-            // 동의
             scores[survey[i].split('')[1]] += Math.abs(choices[i] - 4);
         } 
     }
+    
     function higher(type1, type2){
         let set = '';
-        if(scores[type1] > scores[type2]) {
-            set += type1;
-        } else if(scores[type1] == scores[type2]){
-            set += (type1 > type2 ? type2 : type1);
-        } else {
-            set += type2;
-        }
+        set += scores[type1] >= scores[type2] ? type1 : type2;
         return set;
     }
+    
     result += higher('R', 'T');
     result += higher('C', 'F');
     result += higher('J', 'M');
