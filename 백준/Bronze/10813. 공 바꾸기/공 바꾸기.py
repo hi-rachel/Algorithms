3ab9731575f3df27cf374112a1aa79afe568983e
Rachel <rachel.uiux@gmail.com>
2023-08-08 23:01:19 +0900

    [Bronze II] Title: 공 바꾸기, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,14 +1,9 @@
 n, m = map(int, input().split())
 
-balls = []
-
-for i in range(1, n+1):
-  balls.append(i)
+balls = [*range(1, n+1)]
 
 for _ in range(m):
   i, j = map(int, input().split())
-  temp = balls[i-1]
-  balls[i-1] = balls[j-1]
-  balls[j-1] = temp
+  balls[i-1], balls[j-1] = balls[j-1], balls[i-1]
 
 print(*balls)
