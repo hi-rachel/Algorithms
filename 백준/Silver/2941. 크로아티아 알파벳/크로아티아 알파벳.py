bbdb6f9a2941b6e46b79b3236600d0fe95a3d201
Rachel <rachel.uiux@gmail.com>
2023-08-17 22:47:33 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,6 +1,10 @@
-word = input()
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+words = str(input())
 
-for i in croatia:
-  word = word.replace(i, '%')
-print(len(word))
+c_alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+result = 0
+
+for i in c_alpha:
+  words = words.replace(i, '*')
+
+print(len(words))
