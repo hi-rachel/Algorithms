c5724350e1a75951d737a3962846b85eb86ad42e
Rachel <rachel.uiux@gmail.com>
2023-08-29 13:21:13 +0900

    [Silver I] Title: 포도주 시식, Time: 500 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+k = [0] + [int(input()) for _ in range(n)] + [0]
+
+d = [0] * (n+2)
+
+d[1] = k[1]
+d[2] = d[1] + k[2]
+
+for i in range(3, n+1):
+  d[i] = max(d[i - 3] + k[i - 1] + k[i], d[i - 2] + k[i], d[i - 1])
+
+print(d[n])
