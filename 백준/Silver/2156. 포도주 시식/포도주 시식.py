ff0d21ae7e274a440f7bde01fbff139e41469f87
Rachel <rachel.uiux@gmail.com>
2023-08-29 13:24:29 +0900

    [Silver I] Title: 포도주 시식, Time: 416 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -8,6 +8,7 @@ d[1] = k[1]
 d[2] = d[1] + k[2]
 
 for i in range(3, n+1):
-  d[i] = max(d[i - 3] + k[i - 1] + k[i], d[i - 2] + k[i], d[i - 1])
+  d[i] = max(d[i - 3] + k[i - 1] + k[i], d[i - 2] + k[i])
+  d[i] = max(d[i - 1], d[i])
 
 print(d[n])
