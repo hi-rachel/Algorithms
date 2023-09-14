9bc39cc20f4676a1936d5a64b10aa70b572c9a1f
Rachel <rachel.uiux@gmail.com>
2023-09-14 12:51:53 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -1,13 +1,6 @@
-N = int(input())
+n = int(input())
 
-answer = 0
-for a in range(1, N + 1):
-    for b in range(1, N + 1):
-        for c in range(1, N + 1):
-            if a + b + c == N:
-                if c >= b + 2:
-                    if a != 0 and b != 0 and c != 0:
-                        if a % 2 != 1:
-                            answer += 1
-
-print(answer)
+result = 0
+for i in range(2, n - 2, 2):
+    result += (n - i - 2) // 2
+print(result)
