33f21ec1e69fb5d00dbc1893ae83fa855001086d
Rachel <rachel.uiux@gmail.com>
2023-10-26 23:56:09 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 192 ms, Memory: 33972 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -0,0 +1,27 @@
+import sys
+from heapq import heappush, heappop
+
+input = sys.stdin.readline
+n = int(input())
+
+cards = []
+
+for _ in range(n):
+    heappush(cards, int(input()))
+
+if len(cards) == 1:
+    print(0)
+    exit()
+
+cards_num = 0
+while len(cards):
+    sum = 0
+    sum += heappop(cards)
+    if cards:
+        sum += heappop(cards)
+    cards_num += sum
+    if len(cards) == 0:
+        break
+    heappush(cards, sum)
+
+print(cards_num)
