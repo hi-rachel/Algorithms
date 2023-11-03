057c6b7a3feae2766dd27242f7b7959408524811
Rachel <rachel.uiux@gmail.com>
2023-11-03 18:15:16 +0900

    [Gold V] Title: 토마토, Time: 2108 ms, Memory: 106792 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,23 +1,19 @@
+import sys
 from collections import deque
 
-m, n = map(int, input().split())
-tomatoes = []
+input = sys.stdin.readline
 
-for _ in range(n):
-    tomatoes.append(list(map(int, input().split())))
-
-visited = [[0] * (m + 1) for _ in range(n + 1)]
-dates = [[0] * (m) for _ in range(n)]
+m, n = map(int, input().rstrip().split())
+tomatoes = [list(map(int, input().rstrip().split())) for _ in range(n)]
 
 direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 queue = deque()
 
-for i in range(0, n):
-    for j in range(0, m):
+for i in range(n):
+    for j in range(m):
         if tomatoes[i][j] == 1:
             queue.append([i, j])
-            visited[i][j] = 1
 
 while queue:
     x, y = queue.popleft()
@@ -25,23 +21,15 @@ while queue:
         nx = x + dx
         ny = y + dy
         if (0 <= nx < n) and (0 <= ny < m):
-            if visited[nx][ny] != 1:
-                visited[nx][ny] = 1
-                if tomatoes[nx][ny] != -1:
-                    tomatoes[nx][ny] = 1
-                    dates[nx][ny] = dates[x][y] + 1
-                    queue.append([nx, ny])
+            if tomatoes[nx][ny] == 0:
+                tomatoes[nx][ny] = tomatoes[x][y] + 1
+                queue.append([nx, ny])
 
 for i in range(n):
     for j in range(m):
         if tomatoes[i][j] == 0:
             print(-1)
-            exit()
+            sys.exit()
 
-result = 0
-for i in range(n):
-    for j in range(m):
-        if result < dates[i][j]:
-            result = dates[i][j]
-
-print(result)
+days = tomatoes[x][y] - 1
+print(days)
