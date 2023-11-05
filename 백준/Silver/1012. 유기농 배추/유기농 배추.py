5525ae8d7348e700b6327acf8409772b455ddaff
Rachel <rachel.uiux@gmail.com>
2023-11-05 22:48:18 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,29 +1,23 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(100000)
 
 t = int(input())
 
-def BFS(x, y):
-    queue = deque()
-    queue.append((x, y))
+def DFS(x, y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
 
-    while queue:
-        x, y = queue.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
+        if nx < 0 or ny < 0 or nx >= n or ny >= m:
+            continue
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            if graph[nx][ny] == 1:
-                queue.append((nx, ny))
-                graph[nx][ny] = 0
+        if graph[nx][ny] == 1:
+            graph[nx][ny] = 0
+            DFS(nx, ny)
 
 for _ in range(t):
     m, n, k = map(int, input().split())
@@ -37,7 +31,7 @@ for _ in range(t):
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 1:
-                BFS(i, j)
+                DFS(i, j)
                 cnt += 1
 
     print(cnt)
