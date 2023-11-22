a584ddced55d2866da894e8631428bb4e012013e
Rachel <rachel.uiux@gmail.com>
2023-11-22 23:15:39 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+cnt = 0
+
+d = [0] * 41
+d[1] = 1
+d[2] = 1
+
+def fibonacci(n):
+    global cnt
+    for i in range(3, n + 1):
+        d[i] = d[i - 1] + d[i - 2]
+        cnt += 1
+    return d[n]
+
+print(fibonacci(n), cnt)
