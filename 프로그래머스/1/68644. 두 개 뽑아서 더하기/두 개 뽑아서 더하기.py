91bd07ba82e7c38820863de87fa0e91e8f011ab9
Rachel <rachel.uiux@gmail.com>
2024-04-07 21:42:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.55 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,11 +1,8 @@
-# numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 반환해라.
+from itertools import combinations
 
 def solution(numbers):
-    answer = set()
-    
-    for i in range(len(numbers)):
-        for j in range(i+1, len(numbers)):
-            answer.add(numbers[i] + numbers[j])
-
-    return sorted(list(answer))
+    numbers = combinations(numbers, 2)
+    answer = list(set(a + b for a, b in numbers))
+    answer.sort()
+    return answer
     
