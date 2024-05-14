a55226af10f91e4c7c6c078a71e51ead6d516b56
Rachel <rachel.uiux@gmail.com>
2024-05-14 11:05:17 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 38068 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+li = []
+
+heapq.heapify(li)
+
+for _ in range(n):
+    x = int(input()) * -1
+    if x == 0:
+        if len(li) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(li))
+    else:
+        heapq.heappush(li, x)
