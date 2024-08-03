92d284be236bc99e1e8b87168cdf6d7acf9e4c1e
Rachel <rachel.uiux@gmail.com>
2024-08-03 22:57:26 +0900

    [Silver II] Title: 이동하기, Time: 584 ms, Memory: 78604 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.py
@@ -0,0 +1,16 @@
+N, M = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(N)]
+
+candies = 0
+directions = [(1, 0), (0, 1), (1, 1)]
+
+def max_candies(N, M, map):
+    dp = [[0] * (M+1) for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        for j in range(1, M+1):
+            dp[i][j] = map[i-1][j-1] + max(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+
+    return dp[N][M]
+    
+print(max_candies(N, M, map))
