17603f5437751eef04b82ee09534c849c70259a6
Rachel <rachel.uiux@gmail.com>
2025-01-19 19:29:14 +0900

    [Gold V] Title: 두 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -2,25 +2,23 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-solutions = list(map(int, input().split()))
-solutions.sort()
+N_list = sorted(map(int, input().split()))
+result = (0, 0)
+min_sum = float('inf')
 
-left, right = 0, N - 1
-best_sum = float('inf')
-result = (0, 0) 
+left, right = 0, N-1
 
 while left < right:
-    current_sum = solutions[left] + solutions[right]
-
-    if abs(current_sum) < abs(best_sum):
-        best_sum = current_sum
-        result = (solutions[left], solutions[right])
-
-    if current_sum < 0:
-        left += 1
-    elif current_sum > 0:
+    current_sum = N_list[left] + N_list[right]
+    if abs(current_sum) < abs(min_sum):
+        min_sum = current_sum
+        result = (N_list[left], N_list[right])
+    
+    if current_sum > 0:
         right -= 1
+    elif current_sum < 0:
+        left += 1
     else:
         break
-
+        
 print(result[0], result[1])
