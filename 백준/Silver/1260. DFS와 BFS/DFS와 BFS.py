eaa61a6ef988aabbaecb8ce6bc45c6005572746e
Rachel <rachel.uiux@gmail.com>
2025-01-20 23:03:48 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 35472 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,43 @@
-from collections import deque
 import sys
+from collections import deque
 
 input = sys.stdin.readline
 
-n, m, v = map(int, input().split())
+N, M, V = map(int, input().split())
 
-graph = [[] for _ in range(n + 1)]
-visited = [0 for _ in range(n + 1)]
+visited = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
 
-for _ in range(m):
+for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
 for i in graph:
     i.sort()
-
-def dfs(node):
-    visited[node] = 1
-    print(node, end=" ")
-    for nxt in graph[node]:
-        if visited[nxt] != 1:
-            dfs(nxt)
-
-dfs(v)
+    
+def dfs(v):
+    visited[v] = 1
+    print(v, end=' ')
+    
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i)
+dfs(V)
 print()
-
-visited = [0 for _ in range(n + 1)]
-
-def bfs(node):
-    q = deque()
-    q.append(node)
-    visited[node] = 1
-    while q:
-        now = q.popleft()
-        print(now, end=" ")
-        for nxt in graph[now]:
-            if visited[nxt] != 1:
-                visited[nxt] = 1
-                q.append(nxt)
-
-bfs(v)
+visited = [0] * (N + 1)
+            
+def bfs(v):
+    queue = deque()
+    queue.append(v)
+    visited[v] = 1
+    
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+        for i in graph[node]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
+    
+bfs(V)
