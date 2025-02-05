37aedfc9d7f5f2ed7b2b6a3ab5ee6e3338e5f331
raina <rachel.uiux@gmail.com>
2025-02-05 21:04:48 +0900

    [Silver I] Title: 부등호, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -1,30 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(depth, nums):
+    global min_ans, max_ans
+
+    if depth == k + 1:
+        if not min_ans or nums < min_ans:
+            min_ans = nums
+        if not max_ans or nums > max_ans:
+            max_ans = nums
+        return
+
+    for i in range(10): # 0 ~ 9
+        if not visited[i]:
+            if depth == 0 or signs[depth-1] == "<" and nums[-1] < str(i) or signs[depth-1] == ">" and nums[-1] > str(i):
+                visited[i] = True
+                dfs(depth + 1, nums + str(i))
+                visited[i] = False
+
 k = int(input())
 signs = input().split()
 
-visited = [0]*10
-answer = []
+visited = [False] * 10
+min_ans, max_ans = "", ""
 
-def check(a, b, op):
-    if op == '<':
-        if a > b: return False
-    if op == '>':
-        if a < b: return False
-    return True
+dfs(0, "")
 
-def dfs(cnt, num):
-    if cnt == k+1:
-        answer.append(num)
-        return
-    
-    for i in range(10):
-        if visited[i]: continue
-
-        if cnt == 0 or check(num[cnt-1], str(i), signs[cnt-1]):
-            visited[i] = 1
-            dfs(cnt+1, num+str(i))
-            visited[i] = 0
-
-dfs(0, '')
-answer.sort()
-print(answer[-1])
-print(answer[0])
+print(max_ans)
+print(min_ans)
