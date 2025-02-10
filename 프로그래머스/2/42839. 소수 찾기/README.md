9103f54962e2d1a929a535676ffb16b76be5efee
raina <rachel.uiux@gmail.com>
2025-02-10 21:25:00 +0900

    [level 2] Title: 소수 찾기, Time: 10.63 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.39 ms
+메모리: 10.5 MB, 시간: 10.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 12:06:16
+2025년 02월 10일 21:24:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,21 @@
 from itertools import permutations
 
+def is_prime(n):
+    """소수 판별 함수"""
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):  # √n까지만 검사
+        if n % i == 0:
+            return False
+    return True
+
 def solution(numbers):
-    answer = []
-    nums_list = list(numbers)
-    per = []
-    for i in range(1, len(numbers)+1):
-        per += permutations(nums_list, i)
-        new_list = [int(''.join(p)) for p in per]
-        
-    for num in new_list:
-        if num < 2:
-            continue
-        check = True
-        for j in range(2, int(num**0.5)+1):
-            if num % j == 0:
-                check = False
-                break
-        if check:
-            answer.append(num)
-                    
-    return (len(set(answer)))
+    unique_numbers = set()  # 중복 방지를 위해 set 사용
+
+    # 1~N자리의 모든 순열을 생성하여 숫자로 변환
+    for i in range(1, len(numbers) + 1):
+        for p in permutations(numbers, i):
+            unique_numbers.add(int(''.join(p)))  # set을 사용해 중복 제거
+
+    # 소수 개수 반환
+    return sum(1 for num in unique_numbers if is_prime(num))
