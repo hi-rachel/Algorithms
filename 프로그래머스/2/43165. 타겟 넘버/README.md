e3718c8b37eb370e4fd19d7cd8882b5c2d61950f
raina <rachel.uiux@gmail.com>
2025-03-05 12:43:20 +0900

    [level 2] Title: 타겟 넘버, Time: 549.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 35.5 MB, 시간: 21.37 ms
+메모리: 10.3 MB, 시간: 549.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 12:36:35
+2025년 03월 05일 12:43:18
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,15 @@
-
-
 def solution(numbers, target):
-    answer = 0
+    stack = [(0, 0)]
+    cnt = 0
     
-    def dfs(num_idx, num_sum):
-        if num_idx == len(numbers):
-            if num_sum == target:
-                nonlocal answer
-                answer += 1
-        
-        else:
-            dfs(num_idx+1, num_sum+numbers[num_idx])
-            dfs(num_idx+1, num_sum-numbers[num_idx])
+    while stack:
+        idx, total = stack.pop()
+        if idx == len(numbers):
+            if target == total:
+                cnt += 1
+            continue
     
+        stack.append((idx+1, total+numbers[idx]))
+        stack.append((idx+1, total-numbers[idx]))
     
-    dfs(0, 0);
-    return answer
+    return cnt
