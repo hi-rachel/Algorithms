9457879e7d4c08a42a378139db85e7119d04f2ef
Raina <rachel.uiux@gmail.com>
2025-07-03 22:25:53 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -3,19 +3,22 @@
 홀수번째 알파벳은 소문자로 바꾼 문자열을 반환해라
 
 단어별로 짝홀수 인덱스 판단, 첫 글자 => 짝수
+
+TC: O(n)
+SC: O(n)
 """
 
 def solution(s):
     answer = []
-    words = s.split(' ')
     
-    for word in words:
-        temp = ''
-        for i in range(len(word)):
-            if i == 0 or i % 2 == 0:
-                temp += word[i].upper()
+    for word in s.split(' '):
+        temp = []
+        
+        for i, c in enumerate(word):
+            if i % 2 == 0:
+                temp.append(word[i].upper())
             else:
-                temp += word[i].lower()
-        answer.append(temp)
+                temp.append(word[i].lower())
+        answer.append(''.join(temp))
         
     return ' '.join(answer)
