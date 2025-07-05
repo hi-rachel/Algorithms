bd7882afa3e7265214c59429c0c7d70b9218ccec
Raina <rachel.uiux@gmail.com>
2025-07-05 14:00:26 +0900

    [level 3] Title: 입국심사, Time: 543.93 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,15 +1,26 @@
-def solution(n, times):
-    start, end = 1, max(times) * n
-    answer = end
-    
-    while start <= end:
-        mid = (start + end) // 2
-        people = sum(mid // time for time in times)
+"""
+결정 문제 -> 이분 탐색
 
-        if people >= n:
-            answer = mid
-            end = mid - 1
-        else:
-            start = mid + 1
+시간 범위: 1 ~ max(times) * n
+
+TC: O(log(max(times) × n) × m), m = len(times)
+SC: O(1)
+"""
+
+def solution(n, times):
+    left = 0
+    right = max(times) * n
+    answer = right
     
+    while left <= right:
+        mid = (left + right) // 2
+        
+        total = sum(mid // time for time in times)
+        
+        if total >= n:
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+            
     return answer
