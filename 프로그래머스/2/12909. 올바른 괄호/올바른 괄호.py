fb4b7526fd125ef64ecc09eaa82dd08de9de5a96
Raina <rachel.uiux@gmail.com>
2025-07-05 12:22:29 +0900

    [level 2] Title: 올바른 괄호, Time: 6.01 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,7 +1,10 @@
 def solution(s):
-    answer = True
-    
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return True
+    stack = []
+    for c in s:
+        if c == "(":
+            stack.append(c)
+        elif stack:
+            stack.pop()
+        else:
+            return False
+    return not stack
