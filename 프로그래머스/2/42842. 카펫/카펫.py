dd07dbf0bc99131d7cca4df4c6cd4dee8906e10b
Raina <rachel.uiux@gmail.com>
2025-07-08 12:54:18 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -10,5 +10,5 @@ def solution(brown, yellow):
     for h in range(3, int(total ** 0.5) + 1):
         if total % h == 0:
             w = total // h
-            if ((w-2) * (h-2) == yellow):
+            if (w - 2) * (h - 2) == yellow:
                 return [w, h]
