1aa4c51a4eefde8028c14641cd82be811ea2cbae
Raina <rachel.uiux@gmail.com>
2025-07-08 12:53:47 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 9.35 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 04일 22:38:39
+2025년 07월 08일 12:53:45
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,14 @@
-# 갈색 격자의 수, 노란색 격자의 수
-# 카펫의 가로 길이 >= 세로 길이
-# 카펫의 가로, 세로 크기를 순서대로 배열에 담아 반환해라
+"""
+갈색 격자의 수, 노란색 격자의 수
+카펫의 가로 길이 >= 세로 길이
+카펫의 가로, 세로 크기를 순서대로 배열에 담아 반환해라
+"""
 def solution(brown, yellow):
-    answer = []
-    blocks = brown + yellow
+    total = brown + yellow
     
-    for h in range(3, blocks//3+1):
-        if blocks % h == 0:
-            w = blocks//h
+    # 가장 작은 카펫 3 x 3
+    for h in range(3, int(total ** 0.5) + 1):
+        if total % h == 0:
+            w = total // h
             if ((w-2) * (h-2) == yellow):
-                answer = [w, h]
-                break
-    return answer
+                return [w, h]
