8e4bfac5d7696f2725540c74e7c3ffa86d02f4f2
Raina <rachel.uiux@gmail.com>
2025-07-15 20:50:49 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 35100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,42 @@
+import sys
 from collections import deque
 
+input = sys.stdin.readline
+
+# 세로, 가로
 N, M = map(int, input().split())
 
-maze = []
+maze = [list(map(int, input().rstrip())) for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(a, b):
+    queue = deque()
+    queue.append((a, b))
+    visited[a][b] = 1
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if visited[nx][ny] != 1:
+                    if maze[nx][ny] >= 1:
+                        queue.append((nx, ny))
+                        maze[nx][ny] = maze[x][y] + 1
+                        visited[nx][ny] = 1
+
+                if nx == N - 1 and ny == M - 1:
+                    break
+
 
 for i in range(N):
-    maze.append(list(map(int, input())))
+    for j in range(M):
+        if maze[i][j] == 1:
+            bfs(i, j)
 
-def bfs(x, y):
-    while q:
-        x, y = q.popleft()
-        direction = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        if x == (N - 1) and y == (M - 1):
-            return visited[x][y]
-
-        for dx, dy in direction:
-            nx = x + dx
-            ny = y + dy
-            if nx >= 0 and ny >= 0 and nx <= N - 1 and ny <= M - 1:
-                if maze[nx][ny] == 1 and visited[nx][ny] == 0:
-                    visited[nx][ny] = visited[x][y] + 1
-                    q.append((nx, ny))
-
-q = deque([(0, 0)])
-visited = [[0] * M for _ in range(N)]
-visited[0][0] = 1
-print(bfs(0, 0))
+print(maze[N-1][M-1])
